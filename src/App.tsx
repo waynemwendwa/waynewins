c7c7cf67af4a42378229d160836fa3f11f558ed5
waynemwendwa <waynemwendwa04@gmail.com>
2026-09-16 09:23:27 +0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
@@ -14,7 +15,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-
+        <Analytics />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
